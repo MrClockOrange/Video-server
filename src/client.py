20377e9c00fb5c99172ddbb927ecc5af93e50a56
MrClockOrange <mougel.sylvain@gmail.com>
2014-06-15 23:43:54 +0200

    Add basic stuff

---
+++ src/client.py
@@ -0,0 +1,23 @@
+'''
+Created on 15 juin 2014
+
+@author: sylvainmougel
+'''
+
+
+#!/usr/bin/python           # This is client.py file
+
+import socket               # Import socket module
+
+
+def start():
+    s = socket.socket()         # Create a socket object
+    host = '192.168.0.20' # Get local machine name
+    port = 12345                # Reserve a port for your service.
+
+    s.connect((host, port))
+    print s.recv(1024)
+    s.close                     # Close the socket when done
+
+if __name__ == '__main__':
+    
