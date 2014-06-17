cbcec268de53db975985f8258de85fe1273546ff
MrClockOrange <mougel.sylvain@gmail.com>
2014-06-18 00:01:23 +0200

    Recursive movie finding

--- src/server.py
+++ src/server.py
@@ -1,21 +1,41 @@
 #!/usr/bin/python
-'''
-Created on 10 juin 2014
-
-@author: sylvainmougel
-'''
-'''
-Created on 10 juin 2014
-
-@author: sylvainmougel
-'''
-
 import socket               # Import socket module
 import os
+from os.path import isdir, isfile, join
 
-def get_movies():
-    movies_dir = '/home/pi/data/movies'
-    return os.listdir(movies_dir)
+movies_dir = '/home/pi/data/movies'
+
+
+
+
+class Movie(object):
+    
+    def __init__(self, name, path):
+        self.name = name
+        self.path = path
+
+
+class Movies(object):
+    extension = ('avi', 'mkv', 'mp4')
+    
+    def __init__(self, path):
+        movie_list = []
+        
+
+
+def get_movies(path):
+    """
+    return the list of all the movie under a given path
+    """
+    movies_list = []
+    
+    for f in  os.listdir(movies_dir):
+        if isdir(f):
+            movies_list.extend(get_movies(join(path,f)))
+        elif isfile(f):
+            movies_list.append(f) 
+            
+    return movies_list
     
 
 def start():
@@ -30,7 +50,7 @@ def start():
         print "Wait for a connection"
         c, addr = s.accept()     # Establish connection with client.
         print 'Got connection from', addr
-        c.send(str(get_movies()))
+        c.send(str(get_movies(movies_dir)))
         c.close()                # Close the connection
 
 
