8b045f757fd8e59a83692b79eca0dca5451b2dbe
MrClockOrange <mougel.sylvain@gmail.com>
2014-07-20 01:56:38 +0200

    Tes

--- src/server.py
+++ src/server.py
@@ -61,7 +61,7 @@ def get_movies(path):
 
 
 def launch():
-    print subprocess.Popen(["omxplayer", "-o local", "/home/pi/data/movies/Fargo.S01E08.FASTSUB.VOSTFR.HDTV.XviD-ADDiCTiON"], stdout=subprocess.PIPE).stdout.read()
+    print subprocess.Popen(["omxplayer -o local /home/pi/data/movies/Fargo.S01E08.FASTSUB.VOSTFR.HDTV.XviD-ADDiCTiON"], stdout=subprocess.PIPE, shell=True).stdout.read()
 
 
 class Server(threading.Thread):
